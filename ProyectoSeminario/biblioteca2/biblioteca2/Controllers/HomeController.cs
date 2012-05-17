7b38de8fd825f006d21019780c75aa1c0e5ba054
unknown <MILTON@MILTON-VAIO.(none)>
2012-05-17 15:15:51 -0400

    primera presentacion del proyecto

---
+++ ProyectoSeminario/biblioteca2/biblioteca2/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace biblioteca2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "BIENVENIDO USUARIO";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+       
+    }
+}
---
+++ ProyectoSeminario/biblioteca2/biblioteca2/Controllers/perfilusuarioController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace biblioteca2.Controllers
+{
+    public class perfilusuarioController : Controller
+    {
+        //
+        // GET: /perfilusuario/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ViewResult perfil()
+        {
+            return View();
+        }
+
+    }
+}
